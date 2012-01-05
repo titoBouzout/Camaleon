e464b2da931d803dae26a305987f25445924767a
U-TMIL\tito <tito@TMIL.(none)>
2012-01-05 09:16:26 -0300

    Properly use sublime settings API

--- Camaleon.py
+++ Camaleon.py
@@ -37,32 +37,25 @@ class CamaleonCommand(sublime_plugin.WindowCommand):
 
 		# chrome change
 
-		import codecs
 		#check for soda theme if we are going to switch to soda
 		if s.get('camaleon')[next][0].find('Soda') == 0 and False == os.path.isdir(os.path.join(sublime.packages_path(), 'Theme - Soda')):
 			pass
 		else:
-			path = os.path.join(sublime.packages_path(), 'User', 'Global.sublime-settings')
-			content = codecs.open(path, 'r', 'utf-8').read()
-			try:
-				codecs.open(path, 'w+', 'utf-8').write(content.replace(
-																															s.get('camaleon')[current][0], 
-																															s.get('camaleon')[next][0]
-																														))
-			except:
-				codecs.open(path, 'w+', 'utf-8').write(content)
+			sublime_s = sublime.load_settings('Global.sublime-settings')
+			sublime_s.set('theme', s.get('camaleon')[next][0]);
+			sublime.save_settings('Global.sublime-settings')
 
 		# colour scheme change
-		import json
-		path = os.path.join(sublime.packages_path(), 'User', 'Base File.sublime-settings')
-		BaseFile = json.loads(file(path, 'rb').read())
-		BaseFile['color_scheme'] = s.get('camaleon')[next][1]
+		
+		sublime_s = sublime.load_settings('Base File.sublime-settings')
+		sublime_s.set('color_scheme', s.get('camaleon')[next][1]);
+		sublime.save_settings('Base File.sublime-settings')
+
 		s.set('current', next);
 		sublime.save_settings('Camaleon.sublime-settings')
-		file(path, 'wb+').write(json.dumps(BaseFile, indent=1))
 
 class CamaleonRandomColourSchemeCommand(sublime_plugin.WindowCommand):
-	def run(self, type = 'next'):
+	def run(self):
 		schemes = []
 		for scheme in os.listdir(os.path.join(sublime.packages_path(), 'Color Scheme - Default')):
 			if scheme[-7:] == 'tmTheme':
@@ -70,9 +63,8 @@ class CamaleonRandomColourSchemeCommand(sublime_plugin.WindowCommand):
 		from random import choice
 		scheme = choice(schemes)
 		if scheme != '':
-			import json
-			path = os.path.join(sublime.packages_path(), 'User', 'Base File.sublime-settings')
-			BaseFile = json.loads(file(path, 'rb').read())
-			BaseFile['color_scheme'] = "Packages/Color Scheme - Default/"+scheme
-			file(path, 'wb+').write(json.dumps(BaseFile, indent=1))
+			sublime_s = sublime.load_settings('Base File.sublime-settings')
+			sublime_s.set('color_scheme', "Packages/Color Scheme - Default/"+scheme);
+			sublime.save_settings('Base File.sublime-settings')
+
 			sublime.status_message(u'Camaleon : Loaded colour scheme : '+scheme.decode('utf-8'))
--- readme.md
+++ readme.md
@@ -3,19 +3,17 @@ Description
 
 Camaleón plugin allows you to quickly cycle between a combination of user defined chrome themes and colours schemes when pressing F8
 
-By pressing CTRL+F8 a random colour schemes will be chosen
+By pressing CTRL+F8 a random colour scheme will be chosen
 
 Installation 
 ------------------
 
-1 - Install this package via "Package Control" see: http://wbond.net/sublime_packages/package_control
-
-2 - After installation, Set your current colour scheme to "Solarized (Light)" ( to start cycling correctly )
+Install this package via "Package Control" see: http://wbond.net/sublime_packages/package_control
 
 Usage
 ------------------
 
-By pressing F8 you automatically cycle between your colour choices.
+By pressing F8 you will automatically cycle between your colour choices.
 
 Customization of combinations can be done by editing "Camaleon.sublime-settings"
 
